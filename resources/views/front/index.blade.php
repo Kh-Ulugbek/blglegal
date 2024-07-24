f1eb4fc32a59e2427c748ddcf25e57881a268b20
BiNGo_99 <69698177+Kh-Ulugbek@users.noreply.github.com>
2024-07-24 18:05:52 +0500

    minor

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -116,14 +116,18 @@
     <main class="isolate">
 
         <div class="relative isolate overflow-hidden pt-14">
-            <img src="{{ asset('assets/img/546073.jpg') }}" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
-            <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-                <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+            <img src="{{ asset('assets/img/546073.jpg') }}" alt=""
+                 class="absolute inset-0 -z-10 h-full w-full object-cover">
+            <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                 aria-hidden="true">
+                <div
+                    class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                    style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
             </div>
             <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
                 <div class="text-center">
                     <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">BOGORAZ LAW GROUP</h1>
-                    <p class="mt-6 text-lg leading-8 text-white" >WHERE LAW MATTERS</p>
+                    <p class="mt-6 text-lg leading-8 text-white">WHERE LAW MATTERS</p>
                     <div class="mt-10 flex items-center justify-center gap-x-6">
                         <a href="#"
                            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
@@ -137,11 +141,13 @@
         <!-- Feature section -->
         <div class="mx-auto max-w-7xl px-6 sm:mt-20 lg:px-8">
             <div class="mx-auto max-w-2xl lg:text-center">
-                <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Everything you need to
-                    deploy your app</p>
-                <p class="mt-6 text-lg leading-8 text-gray-600">Quis tellus eget adipiscing convallis sit sit eget
-                    aliquet quis. Suspendisse eget egestas a elementum pulvinar et feugiat blandit at. In mi viverra
-                    elit nunc.</p>
+                <p class="mt-2 text-3xl font-bold text-gray-900 sm:text-5xl">WHY CHOOSE US?</p>
+                <p class="mt-6 text-lg leading-8 text-gray-600">
+                    Unlike other old fashioned Law Firms, we utilize cutting edge technology to provide our clients with
+                    optimal results while enabling you to focus on your recovery. In today’s world, technology drives
+                    innovation which gives us an edge to give each and every client white glove service without
+                    sacrificing their case value.
+                </p>
             </div>
             <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
                 <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
