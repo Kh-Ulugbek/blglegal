20a72901c56c7d10b51346d83333870428d1c55c
BiNGo_99 <69698177+Kh-Ulugbek@users.noreply.github.com>
2024-07-31 11:54:07 +0500

    minor

--- resources/views/admin/components/navbar/mobile-navbar.blade.php
+++ resources/views/admin/components/navbar/mobile-navbar.blade.php
@@ -61,46 +61,26 @@
             </div>
 
             <div class="flex flex-shrink-0 items-center px-4">
-                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=cyan&shade=300" alt="Easywire logo">
+                <img class="h-16 w-auto" src="{{ asset('assets/img/logo.png') }}" alt="Easywire logo">
             </div>
             <nav class="mt-5 h-full flex-shrink-0 divide-y divide-cyan-800 overflow-y-auto" aria-label="Sidebar">
                 <div class="space-y-1 px-2">
                     <!-- Current: "bg-cyan-800 text-white", Default: "text-cyan-100 hover:bg-cyan-600 hover:text-white" -->
-                    <a href="#" class="bg-cyan-800 text-white group flex items-center rounded-md px-2 py-2 text-base font-medium" aria-current="page">
+                    <a
+                        wire:navigate
+                        href="{{ route('admin.dashboard') }}" class="bg-cyan-800 text-white group flex items-center rounded-md px-2 py-2 text-base font-medium" aria-current="page">
                         <svg class="mr-4 h-6 w-6 flex-shrink-0 text-cyan-200" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                         </svg>
                         Home
                     </a>
-                    <a href="#" class="text-cyan-100 hover:bg-cyan-600 hover:text-white group flex items-center rounded-md px-2 py-2 text-base font-medium">
-                        <svg class="mr-4 h-6 w-6 flex-shrink-0 text-cyan-200" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    <a
+                        wire:navigate
+                        href="{{ route('admin.intake') }}" class="@if(request()->route()->getName() == 'admin.intake') bg-cyan-800 text-white @else text-cyan-100 hover:bg-cyan-600 hover:text-white @endif group flex items-center rounded-md px-2 py-2 text-base font-medium">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-4 h-6 w-6 flex-shrink-0 text-cyan-200">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
                         </svg>
-                        History
-                    </a>
-                    <a href="#" class="text-cyan-100 hover:bg-cyan-600 hover:text-white group flex items-center rounded-md px-2 py-2 text-base font-medium">
-                        <svg class="mr-4 h-6 w-6 flex-shrink-0 text-cyan-200" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0012 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 01-2.031.352 5.988 5.988 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 01-2.031.352 5.989 5.989 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971z" />
-                        </svg>
-                        Balances
-                    </a>
-                    <a href="#" class="text-cyan-100 hover:bg-cyan-600 hover:text-white group flex items-center rounded-md px-2 py-2 text-base font-medium">
-                        <svg class="mr-4 h-6 w-6 flex-shrink-0 text-cyan-200" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
-                        </svg>
-                        Cards
-                    </a>
-                    <a href="#" class="text-cyan-100 hover:bg-cyan-600 hover:text-white group flex items-center rounded-md px-2 py-2 text-base font-medium">
-                        <svg class="mr-4 h-6 w-6 flex-shrink-0 text-cyan-200" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
-                        </svg>
-                        Recipients
-                    </a>
-                    <a href="#" class="text-cyan-100 hover:bg-cyan-600 hover:text-white group flex items-center rounded-md px-2 py-2 text-base font-medium">
-                        <svg class="mr-4 h-6 w-6 flex-shrink-0 text-cyan-200" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                        </svg>
-                        Reports
+                        Intake
                     </a>
                 </div>
                 <div class="mt-6 pt-6">
@@ -117,56 +97,50 @@
                             </svg>
                             Help
                         </a>
-                        <a href="#" class="group flex items-center rounded-md px-2 py-2 text-base font-medium text-cyan-100 hover:bg-cyan-600 hover:text-white">
-                            <svg class="mr-4 h-6 w-6 text-cyan-200" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
-                            </svg>
-                            Privacy
-                        </a>
-                        <div x-data="{ open: false }">
-                            <button type="button"
-                                    class="group flex items-center rounded-md px-2 py-2 text-sm font-medium leading-6 text-cyan-100 hover:bg-cyan-600 hover:text-white w-full"
-                                    aria-controls="sub-menu-2" @click="open = !open" aria-expanded="false"
-                                    x-bind:aria-expanded="open.toString()">
-                                <svg class="mr-4 h-5 w-5 shrink-0 text-white" x-state:on="Expanded"
-                                     x-state:off="Collapsed"
-                                     :class="{ 'rotate-90 text-gray-500': open, 'text-gray-400': !(open) }"
-                                     viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd"
-                                          d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-                                          clip-rule="evenodd"></path>
-                                </svg>
-                                Notifications
-                            </button>
-                            <ul x-description="Expandable link section, show/hide based on state." class="mt-1 px-2"
-                                id="sub-menu-2"
-                                x-show="open"
-                                x-transition:enter="transition ease-out duration-100"
-                                x-transition:enter-start="transform opacity-0 scale-95"
-                                x-transition:enter-end="transform opacity-100 scale-100"
-                                x-transition:leave="transition ease-in duration-75"
-                                x-transition:leave-start="transform opacity-100 scale-100"
-                                x-transition:leave-end="transform opacity-0 scale-95"
-                                style="display: none;">
+{{--                        <div x-data="{ open: false }">--}}
+{{--                            <button type="button"--}}
+{{--                                    class="group flex items-center rounded-md px-2 py-2 text-sm font-medium leading-6 text-cyan-100 hover:bg-cyan-600 hover:text-white w-full"--}}
+{{--                                    aria-controls="sub-menu-2" @click="open = !open" aria-expanded="false"--}}
+{{--                                    x-bind:aria-expanded="open.toString()">--}}
+{{--                                <svg class="mr-4 h-5 w-5 shrink-0 text-white" x-state:on="Expanded"--}}
+{{--                                     x-state:off="Collapsed"--}}
+{{--                                     :class="{ 'rotate-90 text-gray-500': open, 'text-gray-400': !(open) }"--}}
+{{--                                     viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">--}}
+{{--                                    <path fill-rule="evenodd"--}}
+{{--                                          d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"--}}
+{{--                                          clip-rule="evenodd"></path>--}}
+{{--                                </svg>--}}
+{{--                                Notifications--}}
+{{--                            </button>--}}
+{{--                            <ul x-description="Expandable link section, show/hide based on state." class="mt-1 px-2"--}}
+{{--                                id="sub-menu-2"--}}
+{{--                                x-show="open"--}}
+{{--                                x-transition:enter="transition ease-out duration-100"--}}
+{{--                                x-transition:enter-start="transform opacity-0 scale-95"--}}
+{{--                                x-transition:enter-end="transform opacity-100 scale-100"--}}
+{{--                                x-transition:leave="transition ease-in duration-75"--}}
+{{--                                x-transition:leave-start="transform opacity-100 scale-100"--}}
+{{--                                x-transition:leave-end="transform opacity-0 scale-95"--}}
+{{--                                style="display: none;">--}}
 
-                                <li>
-                                    <a
-                                        class="hover:bg-cyan-600 block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-white">"Profile
-                                        updated" Email</a>
-                                </li>
+{{--                                <li>--}}
+{{--                                    <a--}}
+{{--                                        class="hover:bg-cyan-600 block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-white">"Profile--}}
+{{--                                        updated" Email</a>--}}
+{{--                                </li>--}}
 
-                                <li>
-                                    <a
-                                        class="hover:bg-cyan-600 block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-white">"Create
-                                        Password" Email</a>
-                                </li>
-                                <li>
-                                    <a
-                                        class="hover:bg-cyan-600 block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-white">"New
-                                        Advisor" Email</a>
-                                </li>
-                            </ul>
-                        </div>
+{{--                                <li>--}}
+{{--                                    <a--}}
+{{--                                        class="hover:bg-cyan-600 block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-white">"Create--}}
+{{--                                        Password" Email</a>--}}
+{{--                                </li>--}}
+{{--                                <li>--}}
+{{--                                    <a--}}
+{{--                                        class="hover:bg-cyan-600 block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-white">"New--}}
+{{--                                        Advisor" Email</a>--}}
+{{--                                </li>--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
                     </div>
                 </div>
             </nav>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@ Route::get('intake', [IntakeController::class, 'intakeForm'])->name('intakeForm'
 Route::middleware('auth')->group(function () {
     Route::middleware('role:admin')->prefix('admin')->group(function () {
         Route::get('dashboard', [Admin\IndexController::class, 'index'])->name('admin.dashboard');
+
+        //Intakes
+        Route::get('intake', [Admin\IntakeController::class, 'index'])->name('admin.intake');
+        Route::post('intake/edit/{id}', [Admin\IntakeController::class, 'edit'])->name('admin.intake.edit');
+        Route::delete('intake/delete/{id}', [Admin\IntakeController::class, 'delete'])->name('admin.intake.delete');
     });
 });
 
